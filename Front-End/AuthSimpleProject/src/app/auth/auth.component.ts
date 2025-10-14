2ea2d0df45c7dcf3106ecc8276f94696b5805688
sobhan <sobhan.ziaee73@gmail.com>
2025-10-14 21:40:04 +0330

    update front

--- Front-End/AuthSimpleProject/src/app/auth/auth.component.ts
+++ Front-End/AuthSimpleProject/src/app/auth/auth.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [CommonModule, FormsModule, HttpClientModule],
+  imports: [CommonModule, FormsModule, 
+// TODO: `HttpClientModule` should not be imported into a component directly.
+// Please refactor the code to add `provideHttpClient()` call to the provider list in the
+// application bootstrap logic and remove the `HttpClientModule` import from this component.
+HttpClientModule],
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
