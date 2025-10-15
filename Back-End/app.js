1bcf0e3b3b5194354df2e53cbc6fc99439b08526
Sobhan <ziaey@gmail.com>
2025-10-15 08:26:24 +0330

    fix and install cors

--- Back-End/app.js
+++ Back-End/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const { notFoundError, errorHandler } = require('./utils/error-handling');
 const { AllRouters } = require('./router/index.routes');
 const { default: mongoose } = require('mongoose');
@@ -11,6 +12,7 @@ mongoose.connect('mongodb://localhost:27017/auth-simple-project', {}).then(() =>
         console.error('Error connecting to MongoDB', err);
     });
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
